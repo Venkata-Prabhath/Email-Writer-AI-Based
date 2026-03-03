e8b737501a299193fa76c75add3a0360ec35e4e1
Prabhath <venkataprabhath.bandi@gmail.com>
2026-03-03 22:46:40 +0530

    Email-Writer-AI-Based

---
+++ email-writer(Backend)/src/main/java/com/email/writer/AiBasedEmailWriterApplication.java
@@ -0,0 +1,13 @@
+package com.email.writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBasedEmailWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiBasedEmailWriterApplication.class, args);
+	}
+
+}
